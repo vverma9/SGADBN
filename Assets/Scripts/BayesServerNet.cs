f28d62f12fbe2caee71a39d9416ce2f1d82b625a
Vipin Verma <vverma9@asu.edu>
2024-11-04 14:30:52 -0700

    Added bayes server dlls and sa sample script

---
+++ Assets/Scripts/BayesServerNet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using BayesServer;
+using BayesServer.Inference.RelevanceTree;
+
+public class BayesServerNet : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("InitializeNetwork", 0);
+        License.Validate("c2ca6130-86da-4a05-af35-2510567e1025");
+    }
+    public void InitializeNetwork()
+    {
+        Debug.Log("making network");
+    }
+
+}
